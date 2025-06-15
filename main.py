dbdddfe9bd1ee769b0f7328e6ccc59c5f5b296b9
Kolnak <balzer.philipp@gmail.com>
2025-06-15 22:55:26 +0200

    day 2 commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os, sys
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
 client = genai.Client(api_key=api_key)
 if len(sys.argv) >= 2:
     input_string = sys.argv[1]
@@ -12,9 +12,14 @@ else:
     print("No input detected")
     exit(1)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=input_string)]),
+]
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=input_string
+    model='gemini-2.0-flash-001', contents=messages,
 )
 print(response.text)
-print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+if (len(sys.argv) >= 3) and (sys.argv[2] == "--verbose"):
+    print("User prompt: " + input_string)
+    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
